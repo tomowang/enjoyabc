7971f75e816b77b412a5a3458d01b815c847801e
Tomo Wang <tomo_wang@163.com>
2013-04-17 00:46:04 +0800

    show 3 most recent articles in front

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,30 @@
  * GET home page.
  */
 
+var Topic = require('../model').Topic;
+
 exports.index = function(req, res){
-  res.render('index');
+  Topic.find({}, function(err, docs){
+    if(err){
+      res.render('index', {articles: []});
+      return;
+    }
+    var articles = []
+      , i
+      , j
+      , a;
+    for(i = 0; i < docs.length; i++){
+      a = docs[i].articles;
+      for(j = 0; j < a.length; j++){
+        articles.push(a[j]);
+      }
+    }
+    articles.sort(function(m, n){
+      return m.date - n.date;
+    });
+    articles = articles.slice(articles.length - 3);
+    res.render('index', {
+      articles: articles
+    });
+  });
 };
