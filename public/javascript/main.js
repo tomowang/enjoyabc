dd9ea5d06c0cbff6a8e66d65c78c9ef2d24ede80
tomo wang <tomo_wang@163.com>
2013-12-01 16:15:08 +0800

    img lazy load

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,5 +2,6 @@
   $(document).ready(function() {
     $("a[href='"+ window.location.pathname +"']").addClass("active");
     $('input, textarea').placeholder();
+    $("img.lazy").lazyload();
   });
 })(jQuery);
